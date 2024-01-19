4cd5e9f55b577764c5161b3444087a28e1efe0f2
admin <greinerrodrigues@gmail.com>
2024-01-19 20:46:44 -0300

    Alteração do link do portifólio

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-// Criando a função toggleMode
 function toggleMode() {
-    const html = document.documentElement // Acessando o documento html
+  const html = document.documentElement
 
-    // if (html.classList.contains('light')) { /* Verificando se no html consta uma classe light */
-    //     html.classList.remove('light') /* Se ja possuir, ele remove */
-    // }else{
-    //     html.classList.add('light') /* Se não possuir ele adiciona */
-    // }
+  html.classList.toggle("light")
 
-    // Maneira simplificada de fazer a estruturação de decisão acima
-    html.classList.toggle('light')
+  const img = document.querySelector("#profile img")
 
-    //Substituindo a imgagem de acordo com o modo
-    const img = document.querySelector("#profile img") /* Aqui estamos selecionando a tag html onde contem a imagem */
-
-    if(html.classList.contains('light')) { /* Verificando se no html consta uma classe light */
-        img.getAttribute('src', './assets/avatar.png') /* Se sim, ele adiciona a classe light */
-    }else{
-        img.setAttribute('src', './assets/avatar.png') /* Se não ele mantem a padrão */
-    }
+  if (html.classList.contains("light")) {
+    img.getAttribute("src", "./assets/avatar.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
